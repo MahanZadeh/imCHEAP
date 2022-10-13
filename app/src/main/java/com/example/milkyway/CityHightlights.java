c56ef32b8c2f1a02495103a63e35d75db119d8d1
Mahan Esmaeilzadeh <mesmaeilzadeh@bcgsc.ca>
2022-10-13 14:39:58 -0700

    Create empty activity for city highlights

---
+++ app/src/main/java/com/example/milkyway/CityHightlights.java
@@ -0,0 +1,14 @@
+package com.example.milkyway;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CityHightlights extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_city_hightlights);
+    }
+}
