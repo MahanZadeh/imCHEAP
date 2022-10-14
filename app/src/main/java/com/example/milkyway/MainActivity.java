1fe3fb6df30c053648257b221f47909213e5ff4a
Mahan Esmaeilzadeh <mesmaeilzadeh@bcgsc.ca>
2022-10-13 19:43:19 -0700

    Add basic structure for the city highlights page

--- app/src/main/java/com/example/milkyway/MainActivity.java
+++ app/src/main/java/com/example/milkyway/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.milkyway;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button toTest = findViewById(R.id.button);
+        toTest.setOnClickListener(view -> {
+            Intent intent = new Intent(this, dynamic_test.class);
+            startActivity(intent);
+        });
     }
+
+
 }
