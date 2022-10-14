edec73317d0d781b997fe6479ffa355fd64d40c4
Mahan Esmaeilzadeh <mesmaeilzadeh@bcgsc.ca>
2022-10-14 11:43:40 -0700

    Cleaned the project

--- app/src/main/java/com/example/milkyway/MainActivity.java
+++ app/src/main/java/com/example/milkyway/MainActivity.java
@@ -13,28 +13,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_up_logo_animation);
 
-
-
-//        Button toTest = findViewById(R.id.button);
-//        toTest.setOnClickListener(view -> {
-//            Intent intent = new Intent(this, dynamic_test.class);
-//            startActivity(intent);
-//        });
-//
-//        Button toLanding = findViewById(R.id.landing);
-//        toLanding.setOnClickListener(view -> {
-//            Intent intent = new Intent(this, StartUpLogoAnimation.class);
-//            startActivity(intent);
-//        });
     }
 
     public void plane(View view) {
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
-//    public void mainActivitySearch(View view) {
-//        Intent searchIntent = new Intent(this, SearchActivity.class);
-//        startActivity(searchIntent);
-//    }
+
 
 }
