de85fb245a339302504ef6c400b9b5a11c993396
hannah-kuo <hannahkuo0205@gmail.com>
2022-10-28 14:13:01 -0700

    Hannah Test

---
+++ app/src/main/java/com/example/milkyway/Hannah_Test.java
@@ -0,0 +1,24 @@
+package com.example.milkyway;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hannah_test);
+    }
+}
