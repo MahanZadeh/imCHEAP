ba42a641dae81b5de89ad60557592c619a13744a
Kelly Olsson <kolsson2002@gmail.com>
2022-11-08 14:37:48 -0800

    Current status of code integration

--- app/src/main/java/com/example/milkyway/SampleAPIconnection.java
+++ app/src/main/java/com/example/milkyway/SampleAPIconnection.java
@@ -30,7 +30,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
-import java.util.stream.Stream;
+import java.util.concurrent.ExecutionException;
 
 
 public class SampleAPIconnection extends AppCompatActivity {
@@ -43,8 +43,8 @@ public class SampleAPIconnection extends AppCompatActivity {
     ArrayList<String> cities = new ArrayList<>();
     HashMap<Double, List<String>> cityInfo = new HashMap<>();
 
-    private final String url = "https://cost-of-living-and-prices.p.rapidapi.com/prices";
-//    private final String appid = "287bb744e2msh06023a70a90dc5cp1b7209jsn23df725e6649";
+    private final String pricesUrl = "https://cost-of-living-and-prices.p.rapidapi.com/prices";
+    private final String citiesUrl = "https://cost-of-living-and-prices.p.rapidapi.com/cities";
     DecimalFormat df = new DecimalFormat("#.##");
 
     @Override
@@ -66,23 +66,80 @@ public class SampleAPIconnection extends AppCompatActivity {
             } else if (countryName.equals("")) {
                 tvResult.setText("Country field can not be empty!");
             } else {
-                tempUrl = url + "?country_name=" + countryName + "&city_name=" + cityName;
+                tempUrl = pricesUrl + "?country_name=" + countryName + "&city_name=" + cityName;
+            }
+
+            // Grab cities
+            AsyncTaskRunner runnerCities = new AsyncTaskRunner();
+            try {
+                ArrayList<String> citiesList = runnerCities.execute(citiesUrl).get();
+                Toast.makeText(SampleAPIconnection.this, "Worked!!!", Toast.LENGTH_SHORT).show();
+            } catch (ExecutionException | InterruptedException e) {
+                e.printStackTrace();
             }
 
             // Testing API calls for two cities
-            cities.add("Berlin");
-            cities.add("Hamburg");
-
-            for (String city : cities) {
-                tempUrl = url + "?country_name=" + country + "&city_name=" + city;
-                AsyncTaskRunner runner = new AsyncTaskRunner();
-                runner.execute(tempUrl);
-            }
+//            cities.add("Berlin");
+//            cities.add("Hamburg");
+//
+//            for (String city : cities) {
+//                tempUrl = pricesUrl + "?country_name=" + country + "&city_name=" + city;
+//                AsyncTaskRunnerPrices runner = new AsyncTaskRunnerPrices();
+//                runner.execute(tempUrl);
+//            }
 //            Toast.makeText(SampleAPIconnection.this, tempUrl.toString(), Toast.LENGTH_SHORT).show();
         });
     }
 
-    private class AsyncTaskRunner extends AsyncTask<String, Void, String> {
+    private class AsyncTaskRunner extends AsyncTask<String, Void, ArrayList<String>> {
+
+        ArrayList<String> citiesArrayList = new ArrayList<>();
+
+        @Override
+        protected ArrayList<String> doInBackground(String... strings) {
+            RequestQueue queue = Volley.newRequestQueue(SampleAPIconnection.this);
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, strings[0], null, response -> {
+                try {
+                    JSONArray cities = response.getJSONArray("cities");
+                    String userInputCountry = etCountry.getText().toString().trim();
+                    String city;
+                    String itemName;
+                    StringBuilder citiesStringBuilder = new StringBuilder();
+
+                    for (int i = 0; i < cities.length(); i++) {
+                        JSONObject jsonObjectPrice = cities.getJSONObject(i);
+                        itemName = jsonObjectPrice.getString("country_name");
+                        itemName = itemName.toLowerCase(Locale.ROOT);
+                        if (itemName.contains(userInputCountry)) {
+                            city = jsonObjectPrice.getString("city_name");
+                            citiesArrayList.add(city);
+                        }
+                    }
+                    for (String s : citiesArrayList) {
+                        citiesStringBuilder.append(s).append("\n");
+                    }
+                    tvResult.setText(citiesStringBuilder);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }, error -> Toast.makeText(SampleAPIconnection.this, error.toString(), Toast.LENGTH_SHORT).show()) {
+
+                /** Passing some request headers* */
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    HashMap<String, String> headers = new HashMap<>();
+                    headers.put("X-RapidAPI-Key", "a7a771dd33mshd86919ea5896511p1a2b01jsnd7f94fc97590");
+                    headers.put("X-RapidAPI-Host", "cost-of-living-and-prices.p.rapidapi.com");
+                    return headers;
+                }
+            };
+
+            queue.add(request);
+            return citiesArrayList;
+        }
+    }
+
+    private class AsyncTaskRunnerPrices extends AsyncTask<String, Void, String> {
 
         @Override
         protected String doInBackground(String... strings) {
