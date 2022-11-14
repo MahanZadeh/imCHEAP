0022f4f1d33d9bed84f080a7ae142b26eecd8f0d
Kelly Olsson <kolsson2002@gmail.com>
2022-11-13 16:46:57 -0800

    Remove unused import statements

--- app/src/main/java/com/example/milkyway/ResultsFragment.java
+++ app/src/main/java/com/example/milkyway/ResultsFragment.java
@@ -5,13 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.Objects;
 
 public class ResultsFragment extends Fragment implements ResultsItemClickListener{
 
