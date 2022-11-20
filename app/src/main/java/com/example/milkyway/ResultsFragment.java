22da8643408c16d2c6ee481633050de6d2436b19
Kelly Olsson <kolsson2002@gmail.com>
2022-11-20 12:57:39 -0800

    Clean up unused import statements

--- app/src/main/java/com/example/milkyway/ResultsFragment.java
+++ app/src/main/java/com/example/milkyway/ResultsFragment.java
@@ -5,25 +5,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.Objects;
 
 
 public class ResultsFragment extends Fragment implements ResultsItemClickListener{
