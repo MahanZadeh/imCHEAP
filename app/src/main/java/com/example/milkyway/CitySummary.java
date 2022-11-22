e9f58bc531226eb440e6b63dfcbecbbeadba1e84
hannah-kuo <hannahkuo0205@gmail.com>
2022-11-21 23:09:09 -0800

    Implemented getting random item + price from the prices API

--- app/src/main/java/com/example/milkyway/CitySummary.java
+++ app/src/main/java/com/example/milkyway/CitySummary.java
@@ -37,9 +37,11 @@ public class CitySummary extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_city_summary);
 
+        // City Highlight #1: Domestic beer costs 51.12 TWD!
+
         TableLayout tableLayout1 = findViewById(R.id.table_layout1); // here we grab the tablelayout
 
-        Random random = new Random();
+        Random randomColour = new Random();
         for(int i=1; i<6; i++) {
             TableRow tableRow = new TableRow(this); //making a row
 
@@ -48,56 +50,62 @@ public class CitySummary extends AppCompatActivity {
             // new
             AsyncTaskRunner runner = new AsyncTaskRunner();
             try {
-                ArrayList<String> citiesList = runner.execute(pricesUrl).get();
+                String itemPrice = runner.execute(pricesUrl).get();
                 Toast.makeText(CitySummary.this, "Worked!!!", Toast.LENGTH_SHORT).show();
+
+                String text = "City Highlight #" + i + ": " + itemPrice;  // adding details and params of the text view to be added
+                textView.setText(text); // for the text/images/etc, look at the adapter thing for the city spinner lab
+                textView.setWidth(900); // mahan how do I set this to match parent or something?
+
+                textView.setHeight(100);
+                textView.setGravity(Gravity.CENTER);
+                textView.setBackgroundColor(Color.argb(255, randomColour.nextInt(256), randomColour.nextInt(256), randomColour.nextInt(256)));
+
+                tableRow.addView(textView); // adding the text to the row
+
+                tableLayout1.addView(tableRow); // THIS is where we add the row to the table layout
+
+
             } catch (ExecutionException | InterruptedException e) {
                 e.printStackTrace();
             }
-
-            String text = "City Highlight (API) #" + i;  // adding details and params of the text view to be added
-            textView.setText(text); // for the text/images/etc, look at the adapter thing for the city spinner lab
-            textView.setWidth(900); // mahan how do I set this to match parent or something?
-//            textView.setWidth(ViewGroup.LayoutParams.MATCH_PARENT);
-
-            textView.setHeight(100);
-            textView.setGravity(Gravity.CENTER);
-            textView.setBackgroundColor(Color.argb(255, random.nextInt(256), random.nextInt(256), random.nextInt(256)));
-
-            tableRow.addView(textView); // adding the text to the row
-
-            tableLayout1.addView(tableRow); // THIS is where we add the row to the table layout
         }
         tableLayout1.setForegroundGravity(Gravity.CENTER);
 
     }
 
-    private class AsyncTaskRunner extends AsyncTask<String, Void, ArrayList<String>> {
+    private class AsyncTaskRunner extends AsyncTask<String, Void, String> {
 
-        ArrayList<String> citiesArrayList = new ArrayList<>();
+        String itemPrice;
 
         @Override
-        protected ArrayList<String> doInBackground(String... strings) {
+        protected String doInBackground(String... strings) {
             RequestQueue queue = Volley.newRequestQueue(CitySummary.this);
             JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, strings[0], null, response -> {
                 try {
                     JSONArray prices = response.getJSONArray("prices");
-                    String itemName = "";
+                    int itemId;
                     double tempPrice = 0.00;
-                    String currencyCode = "";
-                    String randomItem = "beer";
+                    String currencyCode;
+                    String itemName;
+                    Random random = new Random();
+                    int randomItemId = random.nextInt(prices.length());
 
                     for (int i = 0; i < prices.length(); i++) {
+                        // City Highlight #1: Domestic beer costs 51.12 TWD!
                         JSONObject jsonObjectPrice = prices.getJSONObject(i);
-                        itemName = jsonObjectPrice.getString("item_name");
-                        if (itemName.toLowerCase(Locale.ROOT)
-                                .contains(randomItem.toLowerCase(Locale.ROOT))) {
+                        itemId = jsonObjectPrice.getInt("good_id");
+                        if (itemId == randomItemId) {
+                            itemName = jsonObjectPrice.getString("item_name");
                             tempPrice = jsonObjectPrice.getDouble("avg");
                             currencyCode = jsonObjectPrice.getString("currency_code");
-                            StringBuilder sb = new StringBuilder(tempPrice + currencyCode);
+                            StringBuilder sb = new StringBuilder(itemName + " costs " + tempPrice
+                                    + " " + currencyCode);
+                            itemPrice = itemName + " costs " + tempPrice + " " + currencyCode;
                             Toast.makeText(CitySummary.this, sb, Toast.LENGTH_SHORT).show();
                             break;
                         }
-                        itemName = "";
+                        itemId = 1;
                     }
 
                 } catch (JSONException e) {
@@ -116,11 +124,7 @@ public class CitySummary extends AppCompatActivity {
             };
 
             queue.add(request);
-            return citiesArrayList;
+            return itemPrice;
         }
-
-//        @Override
-//        protected void onPostExecute(String result) {
-//        }
     }
 }
