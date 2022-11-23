65788e30e61193ab7d8ae4e339f4b399c3a4c7c1
livictor888 <https://github.com/livictor888/COMP3761>
2022-11-22 20:50:30 -0800

    Redo login button

--- app/src/main/java/com/example/milkyway/ProfileFragment.java
+++ app/src/main/java/com/example/milkyway/ProfileFragment.java
@@ -1,9 +1,9 @@
 package com.example.milkyway;
 
-import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -12,20 +12,15 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -35,9 +30,12 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.Objects;
+
 
 public class ProfileFragment extends Fragment {
 
+    //private MenuItem logout;
     private Button logout;
     private FirebaseUser user;
     private DatabaseReference reference;
@@ -51,12 +49,13 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+
         // Change fragment when favorites button is clicked
         Button btn_favorites = getView().findViewById(R.id.btn_favorites);
         btn_favorites.setOnClickListener(nextView -> {
@@ -77,9 +76,7 @@ public class ProfileFragment extends Fragment {
             ft.addToBackStack("name"); // name can be null
             ft.commit();
         });
-
         logout = getView().findViewById(R.id.logout);
-
         logout.setOnClickListener(nextView -> {
             FirebaseAuth.getInstance().signOut();;
             startActivity(new Intent(getActivity().getApplicationContext(), LoginActivity.class));
@@ -120,5 +117,4 @@ public class ProfileFragment extends Fragment {
         mViewModel = new ViewModelProvider(this).get(ProfileViewModel.class);
         // TODO: Use the ViewModel
     }
-
 }
