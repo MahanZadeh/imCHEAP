e189f115c24595d4dde05f3299731329ecd706ce
Kelly Olsson <kolsson2002@gmail.com>
2022-11-26 18:31:34 -0800

    Remove unused import statement

--- app/src/main/java/com/example/milkyway/SearchFragment.java
+++ app/src/main/java/com/example/milkyway/SearchFragment.java
@@ -41,7 +41,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Random;
 import java.util.stream.Collectors;
 
