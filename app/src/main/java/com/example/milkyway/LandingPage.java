c9c52b4db678ac5d629eb1f1e8697219e5ff916e
Mahan Esmaeilzadeh <mesmaeilzadeh@bcgsc.ca>
2022-11-26 16:18:18 -0800

    Fix warnings

--- app/src/main/java/com/example/milkyway/LandingPage.java
+++ app/src/main/java/com/example/milkyway/LandingPage.java
@@ -2,21 +2,18 @@ package com.example.milkyway;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.os.CountDownTimer;
-import android.view.MenuItem;
 import android.os.Handler;
 import android.view.View;
 import android.widget.ProgressBar;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
 
 public class LandingPage extends AppCompatActivity{
     private ProgressBar progressBar;
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,40 +23,32 @@ public class LandingPage extends AppCompatActivity{
 
 
         final Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                // Do something after 5s = 5000ms
-                getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.fragment_container, new SearchFragment())
-                        .commit();
-                progressBar.setVisibility(View.GONE);
-            }
-        }, 2000); //can also remove the progressbar and set this timer to 1
+        handler.postDelayed(() -> {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new SearchFragment())
+                    .commit();
+            progressBar.setVisibility(View.GONE);
+        }, 2000);
 
         BottomNavigationView bottomNavigationView= findViewById(R.id.bottom_navigation);
 
 
-        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @SuppressLint("NonConstantResourceId")
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Fragment selectedFragment = null;
-                switch (item.getItemId()) {
-                    case R.id.search_panel:
-                        selectedFragment = new SearchFragment();
-                        break;
-                    case R.id.profile_panel:
-                        selectedFragment = new ProfileFragment();
-                        break;
-                    default:
-                }
-
-                if (selectedFragment != null) {
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
-                }
-                return false;
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            switch (item.getItemId()) {
+                case R.id.search_panel:
+                    selectedFragment = new SearchFragment();
+                    break;
+                case R.id.profile_panel:
+                    selectedFragment = new ProfileFragment();
+                    break;
+                default:
             }
+
+            if (selectedFragment != null) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            }
+            return false;
         });
     }
 
